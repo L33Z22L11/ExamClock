d439f6cc1a274a3b2c7a724037a81a0fe9e66df7
L33Z22L11 <L33Z22L11@gmail.com>
2022-03-21 15:57:06 +0800

    优化结构

--- search.js
+++ search.js
@@ -54,3 +54,5 @@ else if (today.date.match("2022-01-2(0|1)")) subject.to(21);
 else subject.to(30);
 // 若不再包一层，slogan.update内的this就会指向window
 setInterval(function () { slogan.update(); }, 2000);
+// 清明节、公祭日黑白滤镜
+if (today.date.match("-04-01|-12-13"))document.documentElement.style.filter="grayscale(0.75)";
