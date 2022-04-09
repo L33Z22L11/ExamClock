794f2d130afc86f18ad7ecef0ded0637011e8391
L33Z22L11 <L33Z22L11@gmail.com>
2022-04-09 16:56:35 +0800

    修正图源 优化动画

--- cover.js
+++ cover.js
@@ -9,7 +9,7 @@ function playCover(msg) {
   setTimeout(function () { eleCoverbar.style.width = "94%"; }, 500);
   setTimeout(function () { eleCoverbar.style.width = "50%"; }, 1000);
   setTimeout(function () { eleCoverbar.style.width = "0.7em"; }, 1500);
-  setTimeout(function () { eleCoverbar.style.width = ""; }, 2500);
-  setTimeout(function () { eleCover.style.opacity = "0"; }, 3000);
-  setTimeout(function () { eleCover.style.display = ""; }, 3500);
+  setTimeout(function () { eleCoverbar.style.width = ""; }, 2000);
+  setTimeout(function () { eleCover.style.opacity = "0"; }, 2500);
+  setTimeout(function () { eleCover.style.display = ""; }, 3000);
 }
