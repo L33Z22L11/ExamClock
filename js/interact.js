cb3515ec86644f2fc36f05ac480c4847166f9368
Zhilu <hi@zhilu.cyou>
2022-08-22 10:05:53 +0800

    开学，更改年级

---
+++ js/interact.js
@@ -0,0 +1,74 @@
+/* 
+ * 页面交互
+ */
+var eleMain = document.getElementById("main"),
+  eleCP = document.getElementById("cp"),
+  eleCPBD = document.getElementById("cpbd"),
+  eleSizebar = document.getElementById("sizebar"),
+  eleContrastbar = document.getElementById("contrastbar"),
+  eleFullscreen = document.getElementById("fullscreen");
+// 键盘功能函数
+onkeydown = function (e) {
+  switch (e.key) {
+    // 隐藏右键菜单
+    case "Escape": try { eleCP.style.display = "none"; } catch (e) { } break;
+  }
+};
+// 右键控制中心
+function displayCP() {
+  eleCP.style.display = "";
+  eleCP.style.opacity = eleCPBD.style.opacity = 0;
+  setTimeout(function () { eleCP.style.opacity = ""; }, 0);
+  setTimeout(function () { eleCPBD.style.opacity = ""; }, 50);
+}
+function hideCP() {
+  eleCP.style.opacity = 0;
+  setTimeout(function () {
+    eleCP.style.display = "none";
+    eleCP.style.opacity = "";
+  }, 200);
+}
+eleMain.oncontextmenu = function (e) {
+  if (e.ctrlKey) { e.preventDefault(); displayCP(); }
+};
+eleCPBD.onclick = function (e) { hideCP(); }
+eleSizebar.firstElementChild.style.width = (parseInt(eleMain.style.fontSize) - 0.75) * 200 + "%";
+eleContrastbar.firstElementChild.style.width = (eleMain.style.opacity - 0.5) * 200 + "%";
+eleSizebar.onmousedown = eleSizebar.ontouchstart =
+  eleSizebar.onmousemove = eleSizebar.ontouchmove =
+  eleSizebar.ondrag = function (e) {
+    e.preventDefault();
+    if (e.buttons == 1) {
+      eleCPBD.style.opacity = 0;
+      eleSizebar.firstElementChild.style.width = e.offsetX + "px";
+      eleMain.style.fontSize = (e.offsetX / eleSizebar.clientWidth) / 2 + 0.75 + "em";
+    }
+  };
+eleContrastbar.onmousedown = eleContrastbar.ontouchstart =
+  eleContrastbar.onmousemove = eleContrastbar.ontouchmove =
+  eleContrastbar.ondrag = function (e) {
+    e.preventDefault();
+    if (e.buttons == 1) {
+      eleCPBD.style.opacity = 0;
+      eleContrastbar.firstElementChild.style.width = e.offsetX + "px";
+      eleMain.style.opacity = (e.offsetX / eleContrastbar.clientWidth) / 2 + 0.5;
+    }
+  };
+// eleSizebar.ondragstart = eleContrastbar.ondragstart = function (e) {
+//   e.preventDefault();
+// }
+onmouseup = function (e) { eleCPBD.style.opacity = ""; };
+// 全屏
+function fullscreen() {
+  try {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+      eleFullscreen.setAttribute("class", "fa-solid fa-expand");
+      eleFullscreen.setAttribute("data-sub", "全屏");
+    } else {
+      document.documentElement.requestFullscreen();
+      eleFullscreen.setAttribute("class", "fa-solid fa-compress");
+      eleFullscreen.setAttribute("data-sub", "退出");
+    }
+  } catch (e) { console.warn(send("操作失败，请手动最大化窗口或全屏。<span class='dim'>建议使用Chrome/Edge/Firefox浏览器。</span>\n") + e); }
+}
---
+++ js/search.js
@@ -0,0 +1,58 @@
+try {
+  var SP = Object.fromEntries(new URLSearchParams(location.search).entries());
+}
+catch (e) { var SP = {}; }
+var TOS = isNaN(SP.tos) ? 0 : --SP.tos;
+// 篡改与浏览器检测
+!function verify() {
+  try {
+    function ifwarn(condition, warn) {
+      if (condition) {
+        document.getElementById("verify").style.display = "block";
+        document.getElementById("verifycontent").innerHTML = warn;
+      }
+    }
+    ifwarn(navigator.userAgent.match(" Trident| QQBrowser"), "IE/QQ浏览器功能老旧，我们推荐使用Chrome/Edge/Firefox浏览器。");
+    ifwarn(!location.host.match("exam.cooo.site|ec.vercel.app"), "您可能在使用第三方或离线的考试时钟，无法保证内容时效性。<u><a href='https://exam.cooo.site '>点击访问考试时钟在线官网 https://exam.cooo.site </a></u>");
+    ifwarn(SP.debug, "您目前处于科目检查(调试)模式。<u><a href='https://exam.cooo.site '>点击返回正常模式</a></u>");
+  }
+  catch (e) {
+    alert("检测到非法篡改，请将此代码发送给纸鹿:\n" + e);
+    if (confirm("是否访问在线考试时钟官网https://exam.cooo.site?")) location.href = "https://exam.cooo.site";
+  }
+  setTimeout(verify, 2000);
+}();
+// 正常或调试模式
+if (SP.debug) {
+  now = new Date(0);
+  document.getElementById("bar").style.transition = "none";
+  !function update() {
+    // 用加号会直接连接字符串，所以这里得减去负数，太魔幻了
+    // 跳转到科目开始前一小时
+    if (now < subject.start - 36E5) now = new Date(subject.start - 36E5);
+    // 最后一场科目结束后重置时间
+    if (now > subject.end - -36E5) subject.to(subject.on);
+    // 调试模式速度设置
+    now.setSeconds(now.getSeconds() + 30);
+    timer.update();
+    setTimeout(update, 25);
+  }();
+} else !function update() {
+  now = new Date;
+  // 设置相对时差
+  now.setSeconds(now.getSeconds() + TOS);
+  timer.update();
+  setTimeout(update, 2000);
+}();
+// 先根据地址参数判断考试类型
+if (SP.type in exam) subject.to(SP.type);
+// 再在考试日期切换到考试类型
+else if (today.date in exam) subject.to(today.date);
+else if (today.date.match("2022-04-1(6|7)")) subject.to(221);
+else if (today.date.match("2022-01-2(0|1)")) subject.to(231);
+// 最后设置缺省考试类型
+else subject.to(23);
+// 若不再包一层，slogan.update内的this就会指向window
+setInterval(function () { slogan.update(); }, 2000);
+// 清明节、公祭日黑白滤镜
+// if (today.date.match("-04-05|-12-13"))document.documentElement.style.filter="grayscale(0.75)";
