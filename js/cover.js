74adecad0c893f97845f71e306dcbc4e4030da76
L33Z22L11 <L33Z22L11@gmail.com>
2023-12-04 01:29:58 +0800

    add phigros-like cover

--- js/cover.js
+++ js/cover.js
@@ -2,15 +2,11 @@
 var eleCover = document.getElementById("cover"),
   eleCoverbar = document.getElementById("coverbar");
 function playCover(msg) {
-  document.getElementById("covermsg").innerHTML = msg;
-  eleCoverbar.style.width = "100%";
+  // document.getElementById("covermsg").innerHTML = msg;
+  // eleCoverbar.style.width = "100%"; 
   eleCover.style.display = "flex";
   setTimeout(function () { eleCover.style.opacity = ""; }, 0);
-  setTimeout(function () { eleCoverbar.style.width = "94%"; }, 300);
-  setTimeout(function () { eleCoverbar.style.width = "50%"; }, 600);
-  setTimeout(function () { eleCoverbar.style.width = "6%"; }, 900);
-  setTimeout(function () { eleCoverbar.style.width = ""; }, 1800);
-  setTimeout(function () { eleCover.style.opacity = "0"; }, 1800);
-  setTimeout(function () { eleCover.style.display = ""; }, 2000);
+  setTimeout(function () { eleCover.style.opacity = "0"; }, 2000);
+  setTimeout(function () { eleCover.style.display = ""; }, 2500);
   return msg;
 }
