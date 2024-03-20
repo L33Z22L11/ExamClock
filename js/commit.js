d217229d64efae1bc916e67589fe478e1045491a
L33Z22L11 <L33Z22L11@gmail.com>
2024-03-20 23:56:30 +0800

    feat: commit time& 月考一

---
+++ js/commit.js
@@ -0,0 +1,17 @@
+/* 作者：纸鹿/Zhilu (github.com/L33Z22L11)
+获取某个 GitHub Repository 的上一次 commit 时间，使用 WTFPL 协议开源。
+使用方式参考：
+<script src="/path-to-commit.js"
+  onload="getLastCommit(document.getElementById('lastCommit'), 'L33Z22L11/ExamClock')"></script>
+ */
+function getLastCommit(ele, repo) {
+  fetch(`https://api.github.com/repos/${repo}/commits?per_page=1`)
+    .then(response => response.json())
+    .then(data => {
+      ele.textContent = `${new Date(data[0].commit.committer.date).toLocaleString()}`
+    })
+    .catch(error => {
+      ele.textContent = `(获取失败)`
+      ele.setAttribute('title', error)
+    })
+}
