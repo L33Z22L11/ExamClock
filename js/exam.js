6d9ed493ecee709deae98b7af435a6bf214ab6df
L33Z22L11 <L33Z22L11@gmail.com>
2024-10-10 19:58:42 +0800

    exam: 高一日常
    Co-authored-by: yearnstudio <yearnmail@qq.com>

--- js/exam.js
+++ js/exam.js
@@ -134,4 +134,44 @@ exams[301] = {
     $("上午", today.date, "08:30", "11:30");
     $("下午", today.date, "14:00", "17:00");
   }
+};
+
+exams[27] = {
+  type: "高一日常",
+  author: "来人维护啊",
+  origin: "阳毅@yearnstudio",
+  mainSlogan: `以梦为马，不负韶华`,
+  rollSlogan: [""],
+  schedule() {
+    // 确实挺难受的
+    $("晨读1", today.date, "07:05", "07:20");
+    $("晨会", today.date, "07:20", "07:25");
+    $("晨读2", today.date, "07:25", "07:45");
+    $("第1节", today.date, "07:55", "08:35");
+    $("第2节", today.date, "08:45", "09:25");
+    $("课间操", today.date, "09:25", "09:50", "<a href='https://mp.weixin.qq.com/s/61Voi3s4tFtAup2-eOg1Lg'><i class='fa-regular fa-circle-play'></i> 点击播放高清室内操", ["《鳌拜鳌拜鳌拜拜》(原曲《Batte Forte》)《夜空中最亮的星》《虫儿飞》"]);
+    $("第3节", today.date, "09:50", "10:30");
+    $("第4节", today.date, "10:40", "11:20");
+    $("第5节", today.date, "11:30", "12:10");
+    if (today.isSummer) {
+      $("第6节", today.date, "14:25", "15:05", null, null, 5);
+      $("第7节", today.date, "15:15", "15:55");
+      $("第8节", today.date, "16:10", "16:50", null, null, 7);
+      $("自习", today.date, "17:00", "18:00");
+      $("晚写", today.date, "19:05", "19:15", null, null, 5);
+      $("晚一", today.date, "19:15", "19:55");
+      $("晚二", today.date, "20:05", "20:45");
+      $("晚三", today.date, "21:00", "22:30", null, null, 7);
+    } else {
+      $("第6节", today.date, "14:10", "14:50", null, null, 5);
+      $("第7节", today.date, "15:00", "15:40");
+      $("第8节", today.date, "15:55", "16:35", null, null, 7);
+      $("自习", today.date, "16:45", "17:45");
+      $("晚写", today.date, "18:45", "18:55", null, null, 5);
+      $("晚一", today.date, "18:55", "19:35");
+      $("晚二", today.date, "19:45", "20:25");
+      $("晚三", today.date, "20:40", "22:10", null, null, 7);
+      $("晚修", today.date, "22:20", "23:30");
+    }
+  }
 }
